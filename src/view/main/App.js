feda4e5697255ab7ff7ccd75c0f9361e4e48267d
Mark Kang <sk531@duke.edu>
2019-11-06 19:23:47 -0500

    main structure added

---
+++ src/view/main/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom'
+
+class App extends React.Component{
+    render(){
+        return(
+            <h1>Works</h1>
+            // <Router>
+            //     <Route exact path= '/' component= {}/>
+            //     <Route path = '/login' component = {}/>
+            //     <Route path = '/signup' component = {}/>
+            //     <Route path = '/home' component = {}/>
+            // </Router>
+        )
+    }
+}
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+var path = require('path');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+module.exports = {
+    entry: {
+      'main' : './src/view/main/index.js',
+    },
+    output: {
+      publicPath: '/'
+    },
+    devServer: {
+      historyApiFallback: true,
+    },
+    plugins:[
+      new HtmlWebPackPlugin({
+        chunks:["main", "vendor"],
+        template: "./src/view/main/index.html",
+        filename: "index.html"
+      })
+    ],
+    module: {
+      rules: [
+        {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader:"babel-loader",
+        options:{
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+          ], 
+        }
+      },
+    },
+
+    {
+      test: /\.css$/,
+      use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
+    },
+    
+  ]
+  },
+  resolve:{
+    alias:{
+       src: path.resolve(__dirname, "src"),
+       components: path.resolve(__dirname, "src", "components")
+    }
+  },
+  optimization: {
+    splitChunks: {
+       cacheGroups: {
+          vendor: {
+             test: /node_modules/,
+             chunks: "initial",
+             name: "vendor",
+             enforce: true
+          }
+       }
+    }
+  }
+}
