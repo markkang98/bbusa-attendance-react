d1418109323acfbd2a0484731d893e70f477cf93
vishmum <vishalm713@gmail.com>
2019-11-10 17:09:02 -0500

    Update Header.js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,7 +5,7 @@ class Header extends React.Component{
     render(){
         return(
             <div className = "header-div">
-                <h1> Vishal is a heasder </h1>
+                <h1> Vishal rocks!</h1>
             </div>
         )
     }
