e330bee332edaf456bfa5164bc77364ff69a28d6
Mark Kang <sk531@duke.edu>
2019-12-02 21:50:34 -0500

    header style created

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from 'react'
 import './Header.css'
-
+import { Link } from 'react-router-dom';
 class Header extends React.Component{
     render(){
         return(
-            <div className = "header-div">
-                <h1>Welcome to Vishal's Dojo</h1>
+            <div className= 'header'>
+                <div className = 'header-left'>
+                    <Link className = 'name' to = '/'>
+                    {/* <img className="logo" src = {require('src/images/logo.png')}/> */}
+                        OurGuider</Link>
+                </div>
+                <div className="header-right">
+                    <Link className = 'signup' to='/signup'>New? Sign Up</Link>
+                    <Link className = 'header-nav' to= '/login'>Log In</Link>
+                </div>
             </div>
         )
     }
--- src/view/home/home.js
+++ src/view/home/home.js
@@ -8,29 +8,14 @@ import { Link } from 'react-router-dom';
 class Home extends React.Component{
     constructor(props){
         super(props)
-        this.state = {message: ''}
+        this.state = {message: '', email: ''}
     }
-    getTrial(){
-        var headers = new Headers();
-        headers.append('testing', "ijackson@hotmail.com");
-         fetch("https://ec2-user@ec2-54-162-54-1.compute-1.amazonaws.com:9443/getClassList",{
-            method: 'GET',
-            credentials: "include",
-            mode: "cors",
-            headers: headers
-        })
-        .then(res => res.text()).then((response)=>{
-            console.log(response)
-            this.setState({message: response})
-        })
-    }
+
 
     render(){
         return(
             <div>
                 <Header/>
-                <button onClick = {this.getTrial.bind(this)}></button>
-                {this.state.message}
                 <Link to = "/login">Log In</Link>
                 <Link to = '/signup'>Sign Up</Link>
             </div>
